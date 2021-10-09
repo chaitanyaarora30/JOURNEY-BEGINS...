888886899362c2e89b730b6db172d07ce6ffd07f
chaitanyaarora30 <60909847+chaitanyaarora30@users.noreply.github.com>
2021-10-09 15:02:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,102 @@
+//package com.PERSONAL;
+
+import java.util.Scanner;
+
+//public class Main {
+
+//    public static void main(String[] args) {
+        // write your code here
+//        System.out.print("HELLO WORLD");
+//    System.out.print("*****\n   *\n  *\n *\n*****");
+//int x = 16;
+//int y = 14;
+//int sum = x + y;
+//int product = x*y;
+//System.out.println("Sum of " + x + " and " + y + " is " + sum);
+//System.out.print("Product of " + x + " and " + y + " is " + product);
+
+//int exp = (x*y)/(x+y);
+//System.out.println(exp);
+
+//        int x = 187;
+//        if (x % 2 == 0)
+//        {
+//                    System.out.print(x + " is even.");
+//        }   else{ System.out.print(x + " is odd");
+//        }
+//int n1 = 78;
+//int n2 = 75;
+//
+//if(n1 == n2)
+//{System.out.print(n1 + " is equal to " + 2);}
+//else
+//{
+//    if(n1>n2)
+//    { System.out.print(n1 + " is greater than " + n2);}
+//    else{ System.out.print(n1 + " is smaller than " + n2);}
+//}
+//
+//Scanner scn = new Scanner(System.in);
+//int marks = scn.nextInt();
+//
+//if (marks > 90)
+//{System.out.print("EXCELLENT");}
+//else if (marks> 80)
+//{System.out.print("GOOD");}
+//else if (marks> 70)
+//{System.out.print("FAIR");}
+//else if (marks> 60)
+//{System.out.print("MEETS EXPECTATIONS");}
+//else if (marks < 60)
+//{System.out.print("BELOW PAR");}
+//
+//        for (int i = 1; i <= 9; i++) {
+//            System.out.println(i);
+//        }
+//int n = scn.nextInt();
+//String name = scn.nextLine();
+//
+//System.out.print("Dear" + name + ". Here is the counting:\n");
+//for(int i = 1; i<= n; i++){
+//    System.out.println(i);}
+//
+//        Scanner scn = new Scanner(System.in);
+//        int n = Integer.parseInt(scn.nextLine());
+//        String name = scn.nextLine();
+//
+//        System.out.print("Dear " + name + ". Here is the counting:\n");
+//        for(int i = 1; i<= n; i++){
+//                System.out.println(i);
+//    }
+//
+//        Scanner scn = new Scanner(System.in);
+//        int t = scn.nextInt();
+//
+//        for(int i = 0; i < t; i++){
+//            int n = scn.nextInt();
+//
+//            for(int div = 1; div <= n; div++){
+//                if(n% div == 0) {
+//                    count++;
+//                }
+//                }
+//            if(count == 2){
+//                System.out.println("PRIME");}
+//            else{
+//                    System.out.println("NOT PRIME");
+//                }
+//
+//            }
+//
+//        }
+//
+//
+//        }
+
+
+
+
+
+
+
+
